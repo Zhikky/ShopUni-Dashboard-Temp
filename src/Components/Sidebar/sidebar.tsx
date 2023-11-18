eab83548152a912487a1ffa3c986f6648186e2bc
Ezekiel Adetuwo <ezeadetuwo@gmail.com>
2023-11-18 14:34:02 +0100

    side_bar completed

--- src/Components/Sidebar/sidebar.tsx
+++ src/Components/Sidebar/sidebar.tsx
@@ -1,16 +1,53 @@
 import React from "react";
 import "./sidebar.scss";
 
+import logo from "../images/FAvicon.png"
+
 function SideBar() {
     return (
         <div className="sidebar_container">
-            <div className="side_buttons">
-                <button><i className="fa-solid fa-chart-line"></i></button>
-                <button><i className="fa-solid fa-dolly"></i></button>
-                <button><i className="fa-solid fa-box-open"></i></button>
-                <button><i className="fa-solid fa-user"></i></button>
+
+            <div className="sidebar_logo">
+                <img src={logo} alt="" width="100px" />
+                {/* <p>ShopUnI</p> */}
             </div>
-            <button className="side_logout"><i className="fa-solid fa-right-from-bracket"></i></button>
+
+            <div className="sidebar_links">
+                <h2>GENERAL</h2>
+                <ul>
+                    <li> <i className="fa-solid fa-chart-line"></i> <p>Dashboard</p></li>
+                    <li> <i className="fa-solid fa-truck-ramp-box"></i> <p>Inventory</p></li>
+                    <li> <i className="fa-solid fa-chart-column"></i> <p>Reports</p></li>
+                </ul>
+            </div>
+
+            <div className="side_support">
+                <h2>SUPPORT</h2>
+                <ul>
+                    <li> <i className="fa-solid fa-circle-info"></i> <p>Help</p></li>
+                    <li> <i className="fa-solid fa-gears"></i> <p>Settings</p></li>
+                    <li> <i className="fa-solid fa-arrow-right-from-bracket"></i> <p>Log out</p></li>
+                </ul>
+            </div>
+
+            <div className="sidebar_footer">
+                <div className="footer_icons">
+                    <a href="">
+                        <i className="fa-brands fa-facebook"></i>
+                    </a>
+                    <a href="">
+                        <i className="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="">
+                        <i className="fa-brands fa-twitter"></i>
+                    </a>
+                    
+                </div> 
+                <p>Privacy Policy</p>
+                <p>Terms & Conditions</p>
+                <p>@2024 ShopUnI</p>
+            </div>
+
         </div>
     )
 };
--- src/Components/dashboard.tsx
+++ src/Components/dashboard.tsx
@@ -1,19 +1,65 @@
 import React from "react";
 import "./dashboard.scss";
 import SideBar from "./Sidebar/sidebar";
+import logoIcon from "./images/zhikky.jpg"
 
 function Dashboard() {
+
+    const productData = [
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        }, {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        },
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        }, {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        },
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        },
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        },
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        },
+        {
+            product: "Cylinder",
+            qunatity: "13",
+            entry_price: "#7,683",
+            exit_price: "#9,542"
+        }
+    ]
+
+
     return (
         <div className="dashboard_container">
             <><SideBar /></>
 
             <div className="dashboard">
-
-                <div>
-                    <h2>Product Dashboard</h2>
-                </div>
-
-                
 
             </div>
         </div>
